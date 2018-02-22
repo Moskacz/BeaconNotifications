7dcfb7f1c901030cf1ee4e98de7a26c9b0d573c6
Michał Moskała <moskala.michal@gmail.com>
2018-02-22 18:24:45 +0100

    Presenting UIImagePickerController in order to choose image

--- BeaconNotifications/ViewController.swift
+++ BeaconNotifications/ViewController.swift
@@ -20,3 +20,31 @@ class ViewController: UIViewController {
         scheduler.startMonitoringBeacons()
     }
 }
+
+// MARK: UI Actions
+extension ViewController {
+    
+    @IBAction func chooseImageTapped(sender: UIButton) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = false
+        present(imagePicker, animated: true, completion: nil)
+    }
+}
+
+extension ViewController: UIImagePickerControllerDelegate {
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : Any]) {
+        guard let image = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            return
+        }
+        
+    }
+}
+
+extension ViewController: UINavigationControllerDelegate {}
