8d077a8429aeda69163cb938bd0e74d45b4c73e3
Michał Moskała <moskala.michal@gmail.com>
2018-02-24 16:37:08 +0100

    Addd AddBeaconRegionViewController with form to add to new BeaconRegion entity

---
+++ BeaconNotifications/AddBeaconRegionViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AddBeaconRegionViewController.swift
+//  BeaconNotifications
+//
+//  Created by Michal Moskala on 24.02.2018.
+//  Copyright © 2018 Michal Moskala. All rights reserved.
+//
+
+import UIKit
+
+class AddBeaconRegionViewController: UIViewController {
+    
+    var viewModel: AddBeaconRegionViewModel?
+}
