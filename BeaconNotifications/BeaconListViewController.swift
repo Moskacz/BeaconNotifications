5a50aedd7964f87975110209e377e9ec2728f7e6
Michał Moskała <moskala.michal@gmail.com>
2018-02-24 16:30:00 +0100

    Displaying list of beacon regions

--- BeaconNotifications/BeaconListViewController.swift
+++ BeaconNotifications/BeaconListViewController.swift
@@ -7,7 +7,63 @@
 //
 
 import UIKit
+import CoreData
 
 class BeaconListViewController: UIViewController {
     
+    var frc: NSFetchedResultsController<BeaconRegion>?
+    
+    @IBOutlet private weak var tableView: UITableView?
+    private let cellReuseId = "beaconCell"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupFRC()
+        setupTableView()
+    }
+    
+    private func setupFRC() {
+        frc?.delegate = self
+    }
+    
+    private func setupTableView() {
+        tableView?.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseId)
+    }
+}
+
+extension BeaconListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return frc?.fetchedObjects?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseId)!
+        let region = frc!.object(at: indexPath)
+        cell.textLabel?.text = [region.beaconId, region.uuid?.uuidString].flatMap { $0 }.joined(separator: " ")
+        let detailsTextComponents = [("major", region.majorValue), ("minor", region.minorValue)]
+        cell.detailTextLabel?.text = detailsTextComponents
+            .filter { $0.1 != 0 }
+            .map { return "\($0.0): \($0.1)"}
+            .joined(separator: ", ")
+        return cell
+    }
+}
+
+extension BeaconListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)
+        cell?.accessoryType = .none
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)
+        cell?.accessoryType = .checkmark
+    }
+}
+
+extension BeaconListViewController: NSFetchedResultsControllerDelegate {
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView?.reloadData()
+    }
 }
