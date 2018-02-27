d7d810c4448a5a0d4955a427686e4e9d158e4cea
Michał Moskała <moskala.michal@gmail.com>
2018-02-27 18:28:06 +0100

    Added isMonitored flag to BeaconRegion entity

--- BeaconNotifications/CoreData/BeaconRegion+CoreDataProperties.swift
+++ BeaconNotifications/CoreData/BeaconRegion+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  BeaconRegion+CoreDataProperties.swift
 //  BeaconNotifications
 //
-//  Created by Michal Moskala on 24.02.2018.
+//  Created by Michal Moskala on 27.02.2018.
 //  Copyright © 2018 Michal Moskala. All rights reserved.
 //
 //
@@ -17,9 +17,10 @@ extension BeaconRegion {
         return NSFetchRequest<BeaconRegion>(entityName: "BeaconRegion")
     }
 
-    @NSManaged public var uuid: UUID?
     @NSManaged public var beaconId: String?
     @NSManaged public var majorValue: Int16
     @NSManaged public var minorValue: Int16
+    @NSManaged public var uuid: UUID?
+    @NSManaged public var isMonitored: Bool
 
 }
